3873f9b21b83ad711affdaf4331b9afb898e961e
djpont <alexandervotyakov@gmail.com>
2023-09-02 04:36:32 +0400

    feat: обновленный прототип игры

--- src/game/index.ts
+++ src/game/index.ts
@@ -1,8 +1,8 @@
 import { Application } from 'pixi.js';
 import { Animations } from '/shared/animation';
-import { BlastLocalization } from '/shared/localozation';
-import { BlastTextures } from '/shared/textures';
-import { Field } from '/components/field';
+import { Localization } from '/shared/localozation';
+import { Textures } from '/shared/textures';
+import { Field } from '/components';
 import { GameController } from './controller';
 import { GameMechanics } from './mechanics';
 
@@ -14,12 +14,12 @@ export class Game {
 
     const showLoading = () => {
       loadingDiv.className = 'loading';
-      loadingDiv.innerText = BlastLocalization.text.loading;
+      loadingDiv.innerText = Localization.text.loading;
       document.body.appendChild(loadingDiv);
     };
 
     const doLoading = async () => {
-      await BlastTextures.loadTextures();
+      await Textures.loadTextures();
     };
 
     const hideLoading = async () => {
@@ -49,16 +49,45 @@ export class Game {
 
   private startBlastGame = () => {
     const app = new Application({
-      width: 800,
-      height: 600,
+      width: window.innerWidth,
+      height: window.innerHeight,
       backgroundColor: 0x1099bb,
     });
     document.body.appendChild(app.view as HTMLCanvasElement);
 
-    const field = new Field(app.stage);
+    const field = new Field();
+    field.controller.addToContainer(app.stage);
 
     const gameMechanics = new GameMechanics(field);
     new GameController(field, gameMechanics);
+
+    // const score = new Panel();
+    // score.controller.addToContainer(app.stage);
+    // score.controller.changeScore(10);
+    // score.controller.changeMovesLeft(20);
+    // const btn1 = new ButtonWeapon(10, BlastLocalization.text.bomb);
+    // btn1.controller.changeProps.position({ x: 100, y: 100 });
+    // btn1.controller.addToContainer(app.stage);
+    // const btn2 = new Button('кнопка', 'purple', 500);
+    // btn2.controller.addToContainer(app.stage);
+    // btn2.controller.changeProps.position({ x: 100, y: 100 });
+
+    // const progress = new ProgressBar(LAYOUT.progress.size, 1);
+    // progress.controller.addToContainer(app.stage);
+    // progress.controller.changeProps.position({ x: 100, y: 25 });
+    //
+    // const pause = new ButtonPause();
+    // pause.controller.addToContainer(app.stage);
+    // pause.controller.registerPixiEvent(GAME.pointerEvent, callback);
+
+    // let money = 100;
+    // const wallet = new Wallet(money);
+    // wallet.controller.addToContainer(app.stage);
+    // wallet.controller.changeProps.position({ x: 50, y: 50 });
+    // wallet.controller.changeCallback(() => {
+    //   money -= 10;
+    //   wallet.controller.changeValue(money);
+    // });
 
     // const a = new NineSlicePlane(BlastTextures.cached.textures.barBackground);
     // a.height = 100;
