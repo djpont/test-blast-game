54dc11b00704ec8f6986f67f1c2225030f8edd69
djpont <alexandervotyakov@gmail.com>
2023-09-02 04:33:47 +0400

    feat: собрал компоненты в единую точку импорта

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { Block } from 'src/components/block';
+export { Button } from 'src/components/button';
+export { ButtonPause } from 'src/components/buttonPause';
+export { ButtonWeapon } from 'src/components/buttonWeapon';
+export { Field } from 'src/components/field';
+export { Panel } from 'src/components/panel';
+export { ProgressBar } from 'src/components/progressBar';
+export { Wallet } from 'src/components/wallet';
