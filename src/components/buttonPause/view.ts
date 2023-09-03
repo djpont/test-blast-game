8f2af44d6f0f148f82d6ffb6156fba0b5ef02b53
djpont <alexandervotyakov@gmail.com>
2023-09-04 02:36:07 +0400

    feat: перенёс изменение курсора в абстрактный класс mvc

--- src/components/buttonPause/view.ts
+++ src/components/buttonPause/view.ts
@@ -6,8 +6,6 @@ import { ButtonPauseModel } from './model';
 export class ButtonPauseView extends MVCView<ButtonPauseModel> {
   constructor(model: ButtonPauseModel) {
     const sprite = new Sprite(Textures.cached.textures.buttonPause);
-    sprite.eventMode = 'static';
-    sprite.cursor = 'pointer';
     super(model, sprite);
   }
 }
