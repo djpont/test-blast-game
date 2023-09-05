715dd933f87865c9023a945143179bcb30bc7893
djpont <alexandervotyakov@gmail.com>
2023-09-05 08:20:51 +0400

    feat: единый экспорт сцен

---
+++ src/scenes/index.ts
@@ -0,0 +1,11 @@
+import { introSceneSchema } from './intro';
+import { secondSceneSchema } from './second';
+import { gameSceneSchema } from './game';
+// import { mainSceneContent } from './main';
+
+export const ScenesSchemas = {
+  intro: introSceneSchema,
+  second: secondSceneSchema,
+  game: gameSceneSchema,
+  // main: mainSceneContent,
+};
